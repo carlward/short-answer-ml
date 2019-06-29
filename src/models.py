c95d35c0db38e9ceed6b477329beeb104fc0aacc
carlward <carlfward@gmail.com>
2019-06-28 21:37:11 -0700

    Update model to ignore padding in seqs using pack_padded_sequence

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class ParaLSTMModel(nn.Module):
@@ -16,20 +17,38 @@ class ParaLSTMModel(nn.Module):
         embeddingLayer.weight.data = embeddings
         for param in embeddingLayer.parameters():
             param.requires_grad = False
-
         return embeddingLayer
 
-    def forward(self, leftSeq, rightSeq):
-        leftEmbeds = self.embeddings(F.pad(leftSeq, pad=(1, 0, 0, 0)))
-        rightEmbeds = self.embeddings(F.pad(rightSeq, pad=(1, 0, 0, 0)))
-        left, em = self.biLSTM(leftEmbeds)
-        right, _ = self.biLSTM(rightEmbeds)
-        pooledLeft = left.max(dim=1).values
-        pooledRight = right.max(dim=1).values
+    def forward(self, inputs):
+        # Sort batch
+        leftLens, sorted_idx_left = inputs.seq1_len.sort(0, descending=True)
+        leftSeq = inputs.seq1[sorted_idx_left]
+        rightLens, sorted_idx_right = inputs.seq2_len.sort(0, descending=True)
+        rightSeq = inputs.seq2[sorted_idx_right]
 
-        return self.dropout(pooledLeft), self.dropout(pooledRight)
+        # Embeddings
+        leftEmbeds = self.embeddings(leftSeq)
+        rightEmbeds = self.embeddings(rightSeq)
 
-    def predict(self, leftSeq, rightSeq):
+        # BiLSTM
+        leftPacked, _ = self.biLSTM(pack_padded_sequence(leftEmbeds, leftLens, batch_first=True))
+        rightPacked, _ = self.biLSTM(pack_padded_sequence(rightEmbeds, rightLens, batch_first=True))
+
+        total_length = max(leftLens[0], rightLens[0])  # Longest seq length overall
+        left, _ = pad_packed_sequence(leftPacked, batch_first=True, total_length=total_length)
+        right, _ = pad_packed_sequence(rightPacked, batch_first=True, total_length=total_length)
+
+        # Max pooling
+        pooledLeft = F.adaptive_max_pool1d(left.permute(0, 1, 2), 1).view(left.size(0), -1)
+        pooledRight = F.adaptive_max_pool1d(right.permute(0, 1, 2), 1).view(right.size(0), -1)
+
+        # Unsort Output
+        _, idx_left = sorted_idx_left.sort(0)
+        _, idx_right = sorted_idx_right.sort(0)
+
+        return self.dropout(pooledLeft[idx_left]), self.dropout(pooledRight[idx_right])
+
+    def predict(self, inputs):
         with torch.no_grad():
-            outputLeft, outputRight = self.forward(leftSeq, rightSeq)
+            outputLeft, outputRight = self.forward(inputs)
             return F.cosine_similarity(outputLeft, outputRight)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,28 +1,22 @@
-from collections import namedtuple
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 
 import torch
 from torch.utils.data import TensorDataset
-
 SEQ_1_COL = '#1 String'
 SEQ_2_COL = '#2 String'
 LABEL_COL = 'Quality'
-
-SeqPair = namedtuple('SeqPair', ['seq1', 'seq2', 'label'])
 
 
 class MRPCDataset(TensorDataset):
     """Microsoft Research Paraphrase Challenge dataset."""
 
-    def __init__(self, root_dir, tokenizer, max_seq_length=200, split='train'):
+    def __init__(self, root_dir, tokenizer, split='train'):
         """
         Args:
             root_dir (string): Directory containing the datasets.
             tokenizer (Tokenizer): Spacy Tokenizer object associated with word embeddings.
-            max_seq_length (int): Max length that sequences will be trimmed/padded to.
             split: (string): Dataset to select. One of {‘train’, ‘test’}
         """
         file_name = 'msr_paraphrase_train.tsv' if split == 'train' else 'msr_paraphrase_test.tsv'
@@ -30,31 +24,37 @@ class MRPCDataset(TensorDataset):
         self.seq_1_col = '#1 String'
         self.seq_2_col = '#2 String'
         self.label_col = 'Quality'
-        self.max_seq_length = max_seq_length
         self.tokenizer = tokenizer
         self.samples = pd.read_csv(self.file_path, sep='\t', quoting=3)
         self.samples[self.label_col] = self.samples[self.label_col].map(lambda v: -1 if v == 0 else v)
 
         # Build Tensors
-        self.tensors = SeqPair(
-            self.seqs_to_tensor(self.samples[self.seq_1_col]),
-            self.seqs_to_tensor(self.samples[self.seq_2_col]),
+        left_seq, left_lens = self.seqs_to_tensor(self.samples[self.seq_1_col])
+        right_seq, right_lens = self.seqs_to_tensor(self.samples[self.seq_2_col])
+
+        self.tensors = (
+            left_seq,
+            left_lens,
+            right_seq,
+            right_lens,
             torch.tensor(self.samples[self.label_col], dtype=torch.long)
         )
 
-    def _prepare_sequence(self, seq):
-        idxs = self._seq_to_ix(seq)
-        return torch.tensor(idxs, dtype=torch.long)
+    def __len__(self):
+        return self.samples.shape[0]
 
     def _seq_to_ix(self, seq):
-        return np.array([token.lex_id for token in self.tokenizer(seq)])
+        return torch.tensor([token.lex_id for token in self.tokenizer(seq)], dtype=torch.long)
 
     def seqs_to_tensor(self, seq_array):
         n_seq = len(seq_array)
-        seq_ix_stack = (self._prepare_sequence(seq) for seq in seq_array)
-        ix_tensor = torch.zeros(n_seq, self.max_seq_length, dtype=torch.long)
+        seq_ix_stack = (self._seq_to_ix(seq) for seq in seq_array)
+        max_seq_length = seq_array.map(len).max()
 
+        ix_tensor = torch.zeros((n_seq, max_seq_length), dtype=torch.long)
+        seq_len_tensor = torch.zeros(n_seq, dtype=torch.long)
         for i, ix in enumerate(seq_ix_stack):
-            ix_tensor[i, :min(len(ix), self.max_seq_length)] = ix + 1
+            ix_tensor[i, :min(ix.size(0), max_seq_length)] = ix
+            seq_len_tensor[i] = ix.size(0)
 
-        return ix_tensor
+        return ix_tensor, seq_len_tensor
